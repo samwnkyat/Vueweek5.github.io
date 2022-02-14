4183c7eca247be0d8701a8f25920f51c4912ba49
samwnkyat <91939489+samwnkyat@users.noreply.github.com>
2022-02-14 18:48:56 +0800

    Update cart.js

--- Week5 Vue延伸套件/cart.js
+++ Week5 Vue延伸套件/cart.js
@@ -1,5 +1,4 @@
 
-import bootstrap from 'bootstrap';
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
